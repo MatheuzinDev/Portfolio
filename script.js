b2f354da6e159b25c1e2e66e524a02f138dfbdad
Matheus Diógenes <matheusdiogenesdev@gmail.com>
2024-08-06 17:40:56 -0300

    atualizações responsividade

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ const elements = document.querySelectorAll('section')
 elements.forEach((element) => {
     myObserver.observe(element)
 })
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('element')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
